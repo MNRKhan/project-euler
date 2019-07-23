116bb6efdc5d0b6737af2d00bbf1e78cdec7da9d
MKhan <md.nahian.99@gmail.com>
2019-07-23 13:21:17 -0400

    Add 009 solution

---
+++ 009/009.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+int main(){
+	int t;
+	cin>>t;
+
+	for(int a0=0;a0<t;a0++){
+		int n;
+		cin>>n;
+		int max=-1;
+		for(int a=1;a<=n-5;a++){	// a=1,a<b<c, therefore, b>=2,c>=3,a<=n-b-c
+			if(n*(n-2*a)%(2*(n-a))==0){
+				int b=(n*(n-2*a))/(2*(n-a));
+				int c=n-a-b;
+				if(max<a*b*c && a<b && b<c)
+					max=a*b*c;
+			}
+		}
+		cout<<max<<endl;
+	}
+	return 0;
+}
+
+
